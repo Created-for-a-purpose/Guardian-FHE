4f0fa8f0efe12fbf827d7b793cb0536ad18b6a26
root <root@THE-SOPHISTICATED>
2023-11-25 14:53:49 +0530

    guest fhe

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -5,32 +5,45 @@ use risc0_zkvm::guest::env;
 use std::io::Read;
 
 use fhe_traits::{DeserializeParametrized, Serialize};
-use fhe::bfv::{Ciphertext, BfvParametersBuilder};
+use fhe::bfv::{Ciphertext, BfvParametersBuilder, BfvParameters};
 
 risc0_zkvm::guest::entry!(main);
 
 #[derive(serde::Serialize, serde::Deserialize)]
-struct FheParam {
-    ciphtxt1: Vec<u8>,
-    ciphtxt2: Vec<u8>,
+struct FhePayParam {
+    sender_cipher_balance: Vec<u8>,
+    ciphertext_amount_sender: Vec<u8>,
+    receiver_cipher_balance: Vec<u8>,
+    ciphertext_amount_receiver: Vec<u8>
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct FhePayResult {
+  sender_cipher_balance: Vec<u8>,
+  receiver_cipher_balance: Vec<u8>
 }
 
 pub fn main() {
-    let parameters = BfvParametersBuilder::new()
-      .set_degree(2048)
-      .set_moduli(&[0x3fffffff000001])
-      .set_plaintext_modulus(1 << 10)
-      .build_arc().unwrap();
+    let parameters =  BfvParameters::default_arc(1, 8);
 
     let mut input_vector = Vec::new();
     env::stdin().read_to_end(&mut input_vector).unwrap();
-    let input: FheParam = bincode::deserialize(&input_vector).unwrap();
+    let input: FhePayParam = bincode::deserialize(&input_vector).unwrap();
 
-    let ciph1: Ciphertext = Ciphertext::from_bytes(&input.ciphtxt1, &parameters).unwrap();
-    let ciph2: Ciphertext = Ciphertext::from_bytes(&input.ciphtxt2, &parameters).unwrap();
+    let sender_cipher_balance: Ciphertext = Ciphertext::from_bytes(&input.sender_cipher_balance, &parameters).unwrap();
+    let receiver_cipher_balance: Ciphertext = Ciphertext::from_bytes(&input.receiver_cipher_balance, &parameters).unwrap();
 
-    let ciph_out = &ciph1 + &ciph2;
-    let result = ciph_out.to_bytes();
+    let ciphertext_amount_sender: Ciphertext = Ciphertext::from_bytes(&input.ciphertext_amount_sender, &parameters).unwrap();
+    let ciphertext_amount_receiver: Ciphertext = Ciphertext::from_bytes(&input.ciphertext_amount_receiver, &parameters).unwrap();
 
-    env::commit(&result);
+    let sender_cipher_new = &sender_cipher_balance - &ciphertext_amount_sender;
+    let receiver_cipher_new = &receiver_cipher_balance + &ciphertext_amount_receiver;
+
+    let result = FhePayResult { 
+      sender_cipher_balance: sender_cipher_new.to_bytes(), 
+      receiver_cipher_balance: receiver_cipher_new.to_bytes() 
+    };
+
+    let serialized_result = bincode::serialize(&result).unwrap();
+    env::commit(&serialized_result);
 }
